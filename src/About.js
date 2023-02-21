67a871131c938acc7ca99e81e62958ee958ea7bb
rajuasynchain <raju@asynchain.com>
2023-02-21 13:41:21 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <main className="About">
+    <h2>About</h2>
+    <p style={{marginTop:"1rem"}}>This Blog App is a project in the laern react </p>
+    </main>
+  )
+}
+
+export default About
